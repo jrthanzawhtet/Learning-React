9163346705928dcf72891f0a7ed35ba417f6424b
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-02-23 14:19:34 +0630

    test

---
+++ ep01-helloworld-react/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import MainMenu from "./components/main-menu";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+
+      <MainMenu></MainMenu>
+    </div>
+    </>
+  );
+}
