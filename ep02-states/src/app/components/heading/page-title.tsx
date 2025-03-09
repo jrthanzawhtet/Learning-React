5bb323cbce8daf2bca3961194fd5fc7b6566e957
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-03-09 14:04:13 +0630

    hello react

---
+++ ep02-states/src/app/components/heading/page-title.tsx
@@ -0,0 +1,5 @@
+export default function PageTitle({title}: {title:string}){
+    return (
+        <h1>{title}</h1>
+    )
+}
---
+++ ep02-states/src/app/simple/page.tsx
@@ -0,0 +1,9 @@
+import PageTitle from "../components/heading/page-title";
+
+export default function SimpleState(){
+    return (
+        <>
+        <PageTitle title= "useState Hook"/>
+        </>
+    )
+}
